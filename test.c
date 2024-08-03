7b5b9a42850b729b54abf0782dad5413493ce65b
bieu-bonfim <gabrielmmbonfim@gmail.com>
2024-08-02 21:10:36 -0300

    grammar update

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef struct {
+    int a;
+    int b;
+} Test;
+
+typedef int (*AddFunc)(int, int);
+
+int add(int a, int b) {
+    return a + b;
+}
+
+void main(int argc, char *argv[]) {
+    
+    float f = .01;
+    double d = .01;
+
+    printf("%f\n", f);
+    printf("%f\n", d);
+
+    Test t;
+    t.a = 1;
+    t.b = 2;
+    printf("t.a = %d, t.b = %d\n", t.a, t.b);
+
+    AddFunc addFunc = add;
+    printf("addFunc(1, 2) = %d\n", addFunc(1, 2));
+
+    
+
+}
