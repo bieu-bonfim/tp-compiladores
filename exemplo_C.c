0d17b3113bd0f03311722be377c334feb44bed3f
Patrickera <patrick.araujo@ufv.br>
2024-08-04 19:09:37 -0300

    Atualização do Enum

---
+++ exemplo_C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int valor1 = 100;
+    int valor2 = 50;
+    int resultado;
+
+    resultado = add(valor1, valor2);
+    return 0;
+}
+
